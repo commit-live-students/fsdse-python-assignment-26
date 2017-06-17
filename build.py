5b809c9632dbe10a178a6746f04564f54f7a9b9f
adinathauti <adinathauti@gmail.com>
2017-06-17 10:32:53 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,9 @@
 def solution(dic1, key1):
-    '''Enter Code Here'''
+    if key1 in dic1.keys():
+        dic1.pop(key1,None)
+    else:
+        pass
+    print dic1
+    return dic1
+
+solution({1:10,2:20},3)
