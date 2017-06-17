94d178bd405883853d3727d2577b29791e04e62a
utk1801 <utk1801@users.noreply.github.com>
2017-06-17 10:17:24 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,11 @@
 def solution(dic1, key1):
     '''Enter Code Here'''
+    for k in dic1:
+        if (k == key1):
+            dic1.pop(key1)
+            break
+    print dic1
+    return dic1
+
+solution({1:10, 2:20, 3:30}, 2)
+solution({1:10, 2:20, 3:30}, 4)
