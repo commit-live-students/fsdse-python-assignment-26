9808e4db78ceb70acf800a0aecb3d27b24f87914
Rohan2015 <rohandamodar@outlook.com>
2017-06-19 08:47:18 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,12 @@
 def solution(dic1, key1):
-    '''Enter Code Here'''
+    popped_key = dic1.pop(key1,None)
+
+    if popped_key != None:
+        #print "popped ",dic1
+        return dic1
+    #print "un-popped",dic1
+    return dic1
+
+dic1 = {1:10, 2:20, 3:30}
+key1 = 4
+solution(dic1, key1)
