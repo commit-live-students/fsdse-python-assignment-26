b1c82a65c331de59cf42b0d0ce381163c700b344
pramodbhalerao <pramodbhalerao_01@yahoo.com>
2017-06-20 14:15:40 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,9 @@
 def solution(dic1, key1):
-    '''Enter Code Here'''
+    my_dict = dic1
+    my_key = key1
+    if my_key in my_dict.keys() :
+        my_dict.pop(my_key)
+    return my_dict
+
+dict_available = {1:10, 2:20, 3:30}
+solution(dict_available,4)
