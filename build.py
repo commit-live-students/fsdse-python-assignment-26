d5fd61c7ebbbea383ba705ba55eb0355646aaf31
Mohammed-Sunasra <Mohammed-Sunasra@users.noreply.github.com>
2017-08-12 14:33:18 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,8 @@
 def solution(dic1, key1):
-    '''Enter Code Here'''
+    if key1 in dic1.keys():
+        del dic1[key1]
+    return dic1
+
+dic = {1:10, 2:20, 3:30}
+key = 2
+print solution(dic,key)
